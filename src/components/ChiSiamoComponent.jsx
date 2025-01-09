2216a2e44d34b496fe51592c8f9defccace362e4
giuse <giuseppedefilippo03@gmail.com>
2025-01-09 16:13:27 +0100

    create component

---
+++ src/components/ChiSiamoComponent.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./NavbarComponent";
+
+function ChiSiamoComponent() {
+    return (
+        <>
+            <Navbar />
+            <p>chi simao? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos quae omnis nemo ex exercitationem. Voluptates et quisquam quibusdam odit, ea fugiat qui enim optio! Sunt soluta velit quisquam dolores quaerat.</p>
+        </>
+    )
+
+}
+export default ChiSiamoComponent;
---
+++ src/components/HomePageComponent.jsx
@@ -0,0 +1,9 @@
+function HomePageComponent() {
+    return (
+        <>
+            <Navbar />
+            <h1>homepage</h1>
+        </>
+    )
+}
+export default HomePageComponent;
